dd3f0c3234bd1aaa707e087c77e662bc10a2b630
Griffin Austin <gaustin25@gmail.com>
2018-08-08 23:30:01 -0600

    HTML Form Support

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
-    body = models.TextField()
+    body = HTMLField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default="default.png", blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
